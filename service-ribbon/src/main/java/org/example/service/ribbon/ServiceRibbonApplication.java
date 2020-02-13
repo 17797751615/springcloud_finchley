61c602db7af35894857a9646be09bbccc18d3ce6
镇四海 <17797751615@163.com>
2020-02-13 22:16:53 +0800

    在 ribbon 中加入histrix

--- service-ribbon/src/main/java/org/example/service/ribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/org/example/service/ribbon/ServiceRibbonApplication.java
@@ -1,11 +1,13 @@
 package org.example.service.ribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @RestController
+@EnableHystrix
 public class ServiceRibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceRibbonApplication.class,args);
@@ -27,7 +30,11 @@ public class ServiceRibbonApplication {
     @Autowired
     RestTemplate resm;
     @GetMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String Hi (String name) {
         return resm.getForObject("http://say-hello/hi?name="+name,String.class);
+    }
+    public String hiError (String name) {
+        return "Hi " + name + "   ,Something worong happed";
     }
 }
